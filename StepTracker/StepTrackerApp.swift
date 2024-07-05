42b29287c344a3e895648400176f9fb16c7129d6
Oscar Santos <oscarsantos.go@gmail.com>
2024-07-05 14:24:14 -0700

    create xcode project

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Oscar Santos on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
